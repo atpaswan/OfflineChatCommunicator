fab1ca9dc22b6080d9309314910df2240d7638ee
atpaswan <atulpaswan_sw@yahoo.in>
2017-02-27 18:44:23 +0530

    Second commit

--- app/src/main/java/wifiemer/tabbedactivity/FragmentMessagesActivity.java
+++ app/src/main/java/wifiemer/tabbedactivity/FragmentMessagesActivity.java
@@ -1,10 +1,12 @@
 package wifiemer.tabbedactivity;
 
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,6 +29,27 @@ public class FragmentMessagesActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tab1messages, container, false);
+        populateListView(rootView);
+        registerClickListener(rootView);
+
+
+        return rootView;
+    }
+
+    public void registerClickListener(final View rootView)
+    {
+        ListView listView=(ListView)rootView.findViewById(R.id.listView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Snackbar.make(rootView,"You have clicked on the laauda item "+position,Snackbar.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void populateListView(View rootView)
+    {
         TextView textView = (TextView) rootView.findViewById(R.id.textView1);
 
         ListView listView=(ListView)rootView.findViewById(R.id.listView);
@@ -35,10 +58,6 @@ public class FragmentMessagesActivity extends Fragment {
         ArrayAdapter<String> adapter=new ArrayAdapter<String>(this.getContext(),R.layout.list_item,strings1);
 
         listView.setAdapter(adapter);
-
-
-
-        return rootView;
     }
 
 }
