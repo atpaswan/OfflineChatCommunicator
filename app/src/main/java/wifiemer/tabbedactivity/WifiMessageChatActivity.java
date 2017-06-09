b97e18f8583ff8cfc7d20a12d5886d2f70af586e
atpaswan <atulpaswan_sw@yahoo.in>
2017-06-09 23:11:50 +0530

    Second commit

--- app/src/main/java/wifiemer/tabbedactivity/WifiMessageChatActivity.java
+++ app/src/main/java/wifiemer/tabbedactivity/WifiMessageChatActivity.java
@@ -16,54 +16,12 @@ public class WifiMessageChatActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wifi_message_chat);
-
-        final Button sendMessage=(Button)findViewById(R.id.sendMessageButton);
-        final Button receivedMessage=(Button)findViewById(R.id.receivedMessageButton);
-        final EditText sendText=(EditText)findViewById(R.id.sendMessageText);
-        final TextView receivedText=(TextView)findViewById(R.id.receivedMessageText);
-        final Button connectClientButton=(Button)findViewById(R.id.connectClientButton);
+        setContentView(R.layout.activity_chat_window);
 
 
-        sendMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                ChatCommunicator chatCommunicator=new ChatCommunicator();
-                String writeString=sendText.getText().toString();
-
-                chatCommunicator.WriteToServer(writeString);
-
-            }
-        });
-
-        connectClientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String hotspotName=getIntent().getStringExtra("HotSpotName");
-
-                WifiHotSpotAccess  wifiHotSpotAccess=new WifiHotSpotAccess();
-
-                if(wifiHotSpotAccess.connectToHotspot(hotspotName,getApplicationContext()))
-                {
-                    System.out.println("HotSpotConnected");
-                }
-                else
-                    System.out.println("Cannot connect to hotspot");
-            }
-        });
 
 
-        receivedMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                (new ChatCommunicator()).readFromClient(receivedText);
-
-
-            }
-        });
 
 
     }
